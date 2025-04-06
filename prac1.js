58ba9415fec3bd302665f01470623dcd8d3f5d84
DimDeveloper18 <dimash.itcout@gmail.com>
2025-04-06 08:08:38 +0100

    Name of the file practice1changed for shorter name prac1

---
+++ prac1.js
@@ -0,0 +1,28 @@
+//console.log("Hello World");
+function mathFunction(x) {
+    return (x ** 2) - (5 * x) + 6;
+}
+
+// console.log(mathFunction (4) );
+
+function milesToKm(miles){
+    return miles * 1.60934;
+}
+// console.log(milesToKm (20))
+
+function quotient(divident, divisor){
+    return (divident - divident % divisor) / divisor;
+}
+// console.log(quotient (17,4))
+
+function farenheitToCelcius(farenheit){
+    return 9 / 5 *(farenheit - 32);
+}
+// console.log(farenheitToCelcius (10));
+
+let str1 = "Hello World"; //Doble quots
+let str2 = "Hellow again"; //Single quots
+
+//Wraping quots
+let quots = 'He sad "No way" and "I heard" something else';
+console.log(quots);

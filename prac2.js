b0f47c6d218188a309a759ad1c0e5b5f1b5a101d
DimDeveloper18 <dimash.itcout@gmail.com>
2025-04-06 08:23:40 +0100

    Prac2 file added for practice lessons

---
+++ prac2.js
@@ -0,0 +1,10 @@
+let instr = "trumpet";
+console.log(instr [0]); //"t"
+console.log(instr [1]); //"r"
+console.log(instr [6]); //"t"
+
+//Indexing work as an array of characters
+
+let account = "123456";
+console.log(accountNumber [2]); //"3"
+console.log(accountNumber.lenght); // "6 totalnumbers of characters"

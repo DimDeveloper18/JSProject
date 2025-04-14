81692f9480b0d4fbb178a167edcbd45ce44788d5
DimDeveloper18 <dimash.itcout@gmail.com>
2025-04-15 00:03:08 +0100

    First array example added

---
+++ arrex.js
@@ -0,0 +1,12 @@
+// Array ex.1 (prints fruit by order)
+/*
+function test(){
+    let arrayOfFruits = ["pear", "apple", "plum", "grape"];
+  //TASK: use the loop below to print the elements
+  //       use 'indexing' also
+  for (let i = 0; i<arrayOfFruits.length; i+=1){
+    console.log(i + ( arrayOfFruits [i] ) );
+  }  
+}
+test();
+ */

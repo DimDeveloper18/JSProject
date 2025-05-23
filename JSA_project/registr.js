366b74321a426e43d686e9e93f2418ca2dd9d4bc
DimDeveloper18 <dimash.itcout@gmail.com>
2025-05-23 17:41:12 +0100

    Loging button function added.

--- JSA_project/registr.js
+++ JSA_project/registr.js
@@ -1,9 +1,24 @@
 // Registration button code
+document.addEventListener("DOMContentLoaded", function (){
 const btnReg = document.getElementById("regBtn");
 const floatForm = document.getElementById("regForm");
+const closeWin = document.getElementById("closeBtn");
+// Login button code
+const btnLog = document.getElementById("logBtn");
+const floatLogForm = document.getElementById ("logForm")
 
 btnReg.addEventListener("click", () => {
     floatForm.style.display = "block";
+});
+
+btnLog.addEventListener("click", () => {
+    floatLogForm.style.display = "block";
+});
+
+closeWin.addEventListener("click", () => {
+    floatForm.style.display = "none";
+});
+
 });
 
 // // Changing color by mouseover the aside head

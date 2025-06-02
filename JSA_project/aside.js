17fc7ee638f1c9ea0a143457f8508ab62a8aeb45
DimDeveloper18 <dimash.itcout@gmail.com>
2025-06-02 01:11:54 +0100

    Aside code complited for items highlighting, and details forms by clicking on item.

--- JSA_project/aside.js
+++ JSA_project/aside.js
@@ -1,25 +1,14 @@
 // Aside JavaScript code
 
-const martic2 = document.getElementsById("martic");
-const asideHead = document.getElementsById("firstChild");
-const sale1 = document.getElementById("bsell1");
-const sale2 = document.getElementById("bsell2");
-const sale3 = document.getElementById("bsell3");
-const sale4 = document.getElementById("bsell4");
+const martic2 = document.getElementById("martic");
+const asideHead = document.getElementById("asHead");
+const sale1 = document.getElementById("battery");
+const sale2 = document.getElementById("drill");
+const sale3 = document.getElementById("skillsaw");
+const sale4 = document.getElementById("cordlessdrill");
 const sale1Wind = document.getElementById("windDet1");
 
-// function createFormSale1(){
-//     const formSale1 = document.createElement("form");
-//     formSale1.textContent = "Product sale details";
-//     formSale1.setAttribute("id", "formS");
-//     formSale1.classList.add("formS");
-//     sale1.appendChild(formSale1);
-// }
-// sale1.addEventListener("click", () => {
-//     createFormSale1();
-// });
-
-// Changing color by mouseover the aside head
+                            // Changing color by mouseover the aside head
 
 asideHead.addEventListener("mouseover", () => {
     asideHead.classList.add("highlight");
@@ -27,17 +16,91 @@ asideHead.addEventListener("mouseover", () => {
 asideHead.addEventListener("mouseout", () => {
     asideHead.classList.remove("highlight");
 });
-// Changing color to Sale item 1
+
+                           // Changing color to Sale item 1
+
 sale1.addEventListener("mouseover", () => {
     sale1.classList.add("highlight");
+});
+sale1.addEventListener("mouseout", () => {
+    sale1.classList.remove("highlight");
+});
+
+document.getElementById("battery").addEventListener("click", (e)=>{
+    if( !e.currentTarget.classList.contains("clicked") ){
+        e.currentTarget.classList.add("clicked");
+    }else{
+        e.currentTarget.classList.remove("clicked");
+    }
+    if( e.currentTarget.classList.contains("clicked") ){
+           e.currentTarget.lastElementChild.style.display="block";
+    }else{
+        e.currentTarget.lastElementChild.style.display="none";
+    }
+});
+
+                            // Changing color to Sale item 2
+
+sale2.addEventListener("mouseover", () => {
+    sale2.classList.add("highlight");
 });
 sale2.addEventListener("mouseout", () => {
     sale2.classList.remove("highlight");
 });
 
-// Open details window by click on the item
-sale1.addEventListener("click", addFunction);
+document.getElementById("drill").addEventListener("click", (e)=>{
+    if( !e.currentTarget.classList.contains("clicked") ){
+        e.currentTarget.classList.add("clicked");
+    }else{
+        e.currentTarget.classList.remove("clicked");
+    }
+    if( e.currentTarget.classList.contains("clicked") ){
+           e.currentTarget.lastElementChild.style.display="block";
+    }else{
+        e.currentTarget.lastElementChild.style.display="none";
+    }
+});
 
-function addFunction(){
-    sale1.classList.add("sale1Wind");
-}
+                            // Changing color to Sale item 3
+
+sale3.addEventListener("mouseover", () => {
+    sale3.classList.add("highlight");
+});
+sale3.addEventListener("mouseout", () => {
+    sale3.classList.remove("highlight");
+});
+
+document.getElementById("skillsaw").addEventListener("click", (e)=>{
+    if( !e.currentTarget.classList.contains("clicked") ){
+        e.currentTarget.classList.add("clicked");
+    }else{
+        e.currentTarget.classList.remove("clicked");
+    }
+    if( e.currentTarget.classList.contains("clicked") ){
+           e.currentTarget.lastElementChild.style.display="block";
+    }else{
+        e.currentTarget.lastElementChild.style.display="none";
+    }
+});
+
+                            // Changing color to Sale item 4
+
+sale4.addEventListener("mouseover", () => {
+    sale4.classList.add("highlight");
+});
+sale4.addEventListener("mouseout", () => {
+    sale4.classList.remove("highlight");
+});
+
+document.getElementById("cordlessdrill").addEventListener("click", (e)=>{
+    if( !e.currentTarget.classList.contains("clicked") ){
+        e.currentTarget.classList.add("clicked");
+    }else{
+        e.currentTarget.classList.remove("clicked");
+    }
+    if( e.currentTarget.classList.contains("clicked") ){
+           e.currentTarget.lastElementChild.style.display="block";
+    }else{
+        e.currentTarget.lastElementChild.style.display="none";
+    }
+});
